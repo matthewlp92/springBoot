155190acb128593e2b46c05bc91ca2e2ccdf21e4
Matteo Sbaragli <matteo.sbaragli@outlook.it>
2024-01-23 11:32:44 +0100

    primo commit

---
+++ src/main/java/com/example/springboot_exercise/SpringBootExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExerciseApplication.class, args);
+    }
+
+}
